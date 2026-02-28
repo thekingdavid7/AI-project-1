86065017e02d2a9e0e0c1e106d8b4d89dd77c565
thekingdavid7 <thekingdavido@gmail.com>
2026-02-28 15:09:57 -0600

    made the boardView class and visualized the board

---
+++ boardView.cpp
@@ -0,0 +1,59 @@
+#include "boardView.hpp"
+
+boardView::boardView(int N)
+{
+    size = N;
+}
+
+void boardView::showBoard()
+{   
+    cout << "-----"; // -------------
+    for (int i = 1; i < size; i++) //top line
+    {
+        cout << "----";
+    }
+
+    cout << endl;
+
+    for (int i = 1; i <= size - 1; i++)
+    {
+        cout << "|"; // |   |   |   |
+        for (int j = 1; i <= size; i++)
+        {
+            cout << "   |";
+        }
+
+        cout << endl;
+
+        cout << "|"; // |---+---+---|
+        for (int j = 1; i < size; i++)
+        {
+            cout << "---+";
+        }
+
+        cout << "---|";
+        cout << endl;
+    }
+
+    cout << "|"; // |   |   |   |
+    for (int i = 1; i <= size; i++)
+    {
+        cout << "   |";
+    }
+    cout << endl;
+    cout << "-----"; // -------------
+    for (int i = 1; i < size; i++) //bottom line
+    {
+        cout << "----";
+    }
+    
+    // -------------
+    // |   |   |   |
+    // |---+---+---|
+    // |   |   |   |
+    // |---+---+---|
+    // |   |   |   |
+    // |---+---+---|
+    // |   |   |   |
+    // -------------
+}
