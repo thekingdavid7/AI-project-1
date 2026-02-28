e7a85303e09f9849fafe06389455d24e8c659e9f
thekingdavid7 <thekingdavido@gmail.com>
2026-02-28 13:21:15 -0600

    set up driver and makefile, takes in arguments

---
+++ connectM.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//connectM N M H
+//N: rows/cols in NxN grid
+//M: how many connected to win
+//H: human or computer first move (1 or 0)
+
+//arg c argument
+//argv is a list of args passedfrom the command line
+int main(int argc, char* argv[])
+{
+    if (argc != 4) //make sure only 4 arguments
+    {
+        cerr << "Usage: " << argv[0] << " <N> <M> <H>" << endl;
+        return 1;
+    }
+
+    //args
+    string N = argv[1]; //rows/cols in NxN grid
+    int M = stoi(argv[2]); //how many connected to win
+    int H = stoi(argv[3]); //0 or 1
+    
+    cout << "hello, world!" << endl;
+    cout << "N : " << N << endl;
+    cout << "M : " << M << endl;
+    cout << "H : " << H << endl;
+
+    return 0;
+}
