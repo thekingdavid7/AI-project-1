d22fd2cd72f4abc54f81c0b019e3b3783928cce1
Ronald99-projects <ronald.odell@hotmail.com>
2026-03-08 08:40:59 -0500

    Game files written

--- game.hpp
+++ game.hpp
@@ -12,11 +12,15 @@ class game
         int size = 0; //N: NxN size of grid
         int winState; //M: how many in a row to win
         vector<vector<char>> board;
+        bool checkDirection(int row, int col, int dRow, int dCol, char piece);
 
     public:
         game(int N, int M);
         vector<vector<char>> getBoard();
         void dropPiece(int player, int x);
+        bool checkDraw();
+        bool checkWin(char piece);
+
 };
 
 #endif
